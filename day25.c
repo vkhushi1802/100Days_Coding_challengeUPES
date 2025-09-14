0bce6bca78630c4e962c6088a2cee9c8c529a9ff
khushi1802@gmail.com <vkhushi1802@gmail.com>
2025-09-14 22:46:51 +0530

    day25

--- day25.c
+++ day25.c
@@ -1,30 +1,35 @@
-// Q47-Write a program to print the following pattern: * ** *** **** *****
+// Q49-Write a program to print the following pattern: 5 45 345 2345 12345
 
-#include <stdio.h>
-int main(){
-    int n= 5;
+// #include <stdio.h>
+// int main(){
+//     int n=5;
+//     for(int i=n; i>=1; i--){
+//         for(int j=i; j<=n; j++){
+//             printf("%d",j);
+//         }
+    
+//         printf("\n");   
+//     }
+//     return 0;
+// }
 
-    for(int i=0; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            printf("*");
-        }
-        printf(" ");
-    }
-    return 0;
-}
 
 
-// Q48-Write a program to print the following pattern: 1 12 123 1234 12345
+// Q50-Write a program to print the following pattern: ***** **** *** ** *
 
-#include <stdio.h>
+#include<stdio.h>
 int main(){
     int n=5;
-
-    for(int i=0; i<=n; i++){
-        for (int j=1; j<=i; j++){
-            printf("%d",j);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            printf(" ");
         }
-        printf(" ");
+
+        for(int j=0; j<n-i; j++){
+            printf("*");
+        }
+        printf("\n");
     }
+
     return 0;
 }
