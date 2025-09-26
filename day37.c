02b24bde058cf54fb96846757e88595f958523f1
khushi1802@gmail.com <vkhushi1802@gmail.com>
2025-09-26 12:39:00 +0530

    day34

--- day37.c
+++ day37.c
@@ -1,27 +1,27 @@
 // Q73-Find the sum of each row of a matrix and store it in an array.
 
-// #include <stdio.h>
-// int main(){
-//     int rows , col;
-//     printf("Enter rows and columns: ");
-//     scanf("%d %d", &rows, &col);
+#include <stdio.h>
+int main(){
+    int rows , col;
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &rows, &col);
 
     
-//     int sumarr[50];
-//     int arr[rows][col];
-//     printf("Enter elements of matrix: ");
-//     for(int i=0; i<rows; i++){
-//         int sum =0;
-//         for(int j=0; j<col; j++){
-//             scanf("%d", &arr[i][j]);
-//             sum += arr[i][j];
-//         }
-//         sumarr[i] = sum;
-//         printf("%d ", sumarr[i]);
-//     }
+    int sumarr[50];
+    int arr[rows][col];
+    printf("Enter elements of matrix: ");
+    for(int i=0; i<rows; i++){
+        int sum =0;
+        for(int j=0; j<col; j++){
+            scanf("%d", &arr[i][j]);
+            sum += arr[i][j];
+        }
+        sumarr[i] = sum;
+        printf("%d ", sumarr[i]);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
