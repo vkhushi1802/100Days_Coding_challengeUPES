a7734d5b74b676a4caa99731de7b8a7d85dfbd19
khushi1802@gmail.com <vkhushi1802@gmail.com>
2025-10-12 21:42:20 +0530

    day-50

---
+++ day50_2.c
@@ -0,0 +1,34 @@
+// Q100-Print all sub-strings of a string.
+
+#include <stdio.h>
+#include <string.h>
+
+void gensubstr(char* str, int st, int end, int len){
+    if(st==len){
+        return;
+    }
+
+    if(end>len){
+        gensubstr(str, st+1, st+1, len);
+        return;
+    }
+
+    for(int i=st; i<end; i++){
+        printf("%c", str[i]);
+    }
+    printf("\n");
+
+    gensubstr(str, st, end+1, len);
+}
+
+int main(){
+    char str[100];
+    printf("Enter a sting: ");
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    gensubstr(str, 0,0, len);
+
+    return 0;
+}
